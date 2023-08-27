a3e6a57afe717fd2c0a266c52ff3ac77d51dfc22
maximo <2mxmcln@gmail.com>
2023-08-27 11:35:48 -0300

    compresores

--- js/main_perifericos.js
+++ js/main_perifericos.js
@@ -3,8 +3,14 @@ let nheader=`
 <section class="menu-menu">
     <nav class="menu-principal">
         <ul>
-            <li><a href="../index.html">Inicio</a></li>
+            <li><a href="../index.html">Inicio</a></li>			
+            <li><a href="CP001 Compresor antiguo.html">CP001 Compresor antiguo</a></li>
+            <li><a href="CP002 Compresor nuevo.html">CP002 Compresor nuevo</a></li>			
             <li><a href="Estrusora.html">Estrusora</a></li>
+            <li><a href="SP001 Manuales.html">SP001 Manuales</a></li>
+            <li><a href="SP002 505.html">SP002 505</a></li>			
+            <li><a href="SP003 306.html">SP003 306</a></li>			
+            <li><a href="SP004 613.html">SP004 613</a></li>						
             <li><a href="CH008 Frimec exm2 pared.html">CH008 Frimec exm2 M1 pared</a></li>
             
         </ul>
